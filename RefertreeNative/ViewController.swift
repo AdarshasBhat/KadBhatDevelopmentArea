cc57ec2eedb0659694006b24216c2c841dc318b8
iamadarshbhat <adarshas@nousinfo.com>
2016-10-03 17:40:43 +0530

    First Commit on Monday

---
+++ RefertreeNative/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  RefertreeNative
+//
+//  Created by Adarsha on 29/09/16.
+//  Copyright © 2016 NousInfosystems. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var menuBtn: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+      
+        if( self.revealViewController() != nil){
+        menuBtn.target = self.revealViewController()
+            menuBtn.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
